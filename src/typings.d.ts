7d149ec83e998176cce0ef6ce46d5f715410847c
BaymaxGroot <lele_it_dog@163.com>
2020-08-11 17:57:24 +0800

    modify the angular.json file

---
+++ src/typings.d.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: xmll
+ * @Date: 2020-08-08 16:04:02
+ * @LastEditTime: 2020-08-08 16:06:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \AngularBasePlatform\src\typings.d.ts
+ */
+// # 3rd Party Library
+// If the library doesn't have typings available at `@types/`,
+// you can still use it by manually adding typings for it
